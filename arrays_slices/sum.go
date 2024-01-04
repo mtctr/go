d72066bf9bb6d8641b9da40604f39f96c38fe1c0
Mateus <mateustargino95@gmail.com>
2024-01-03 21:38:19 -0300

    add sum of slices and tails

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -7,3 +7,32 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+
+		tail := GetTail(numbers)
+		sums = append(sums, Sum(tail))
+	}
+	return sums
+}
+
+func GetTail(numbers []int) []int {
+	if len(numbers) == 0 {
+		return []int{0, 0}
+	}
+	return numbers[1:]
+
+}
